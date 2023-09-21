03d94d5c7d979c2763b7cd1f6aa91fad36bd4c77
Evan <stevanuspangau50@gmail.com>
2023-09-21 13:27:07 +0700

    container stats

---
+++ docker-dasar/7.container-stats.sh
@@ -0,0 +1,6 @@
+# Untuk melihat status container kita bisa menggunakan perintah
+docker container stats
+
+# atau jika ingin melihat stats container tertentu bisa lihat dengan menggunakan perintah
+# docker container stats namaContainer
+docker container stats contohnginx
