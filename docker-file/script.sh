d4ba7ee630e7578ae96e00b393577661739de3f3
Evan <stevanuspangau50@gmail.com>
2023-09-22 13:22:44 +0700

    command

--- docker-file/script.sh
+++ docker-file/script.sh
@@ -6,3 +6,14 @@ docker build -t stevanuspangau/run run
 
 # From Run - memunculkan Display Output seperti cat
 docker build -t stevanuspangau/run run --progress=plain --no-cache
+
+# From Command
+docker build -t stevanuspangau/command command
+
+# From Command - untuk melihat hasil echo ketik perintah
+docker image inspect stevanuspangau/command
+
+# From Command - buat container dari image stevanuspangau/command
+docker container create --name command stevanuspangau/command
+docker container start command
+docker container logs command
