0d402dadc26746377b1c576350cab16a62b1d92d
Evan <stevanuspangau50@gmail.com>
2023-09-22 11:33:47 +0700

    from dan run

---
+++ docker-file/script.sh
@@ -0,0 +1,8 @@
+# From Instruction
+docker build -t stevanuspangau/from from
+
+# From Run
+docker build -t stevanuspangau/run run
+
+# From Run - memunculkan Display Output seperti cat
+docker build -t stevanuspangau/run run --progress=plain --no-cache
